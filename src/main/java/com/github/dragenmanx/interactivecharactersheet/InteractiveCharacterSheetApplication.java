fe704d283b122180b4c322e83e98da07b408e6b5
TStoykov-Proxiad <t.stoykov@proxiad.com>
2023-01-23 19:08:34 +0200

    [] init commit

---
+++ src/main/java/com/github/dragenmanx/interactivecharactersheet/InteractiveCharacterSheetApplication.java
@@ -0,0 +1,13 @@
+package com.github.dragenmanx.interactivecharactersheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteractiveCharacterSheetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteractiveCharacterSheetApplication.class, args);
+	}
+
+}
